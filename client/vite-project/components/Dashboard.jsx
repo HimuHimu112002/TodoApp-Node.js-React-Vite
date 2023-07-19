9845bfaec9328bd4227c711584305ff5d897f3d5
HimuHimu112002 <mdhmaktaruzzaman9101@gmail.com>
2023-07-19 22:25:53 +0600

    code

--- client/vite-project/components/Dashboard.jsx
+++ client/vite-project/components/Dashboard.jsx
@@ -2,11 +2,30 @@ import React from 'react'
 import { useEffect, useState } from "react"
 import axios from 'axios';
 import { Col, Container, Row } from 'react-bootstrap';
-
+import {
+    PieChart,
+    Pie,
+    Tooltip,
+    BarChart,
+    XAxis,
+    YAxis,
+    Legend,
+    CartesianGrid,
+    Bar,
+  } from "recharts";
 const Dashboard = () => {
     let [list, setList] = useState([])
     let [Cancellist, setCancelList] = useState([])
     let [Approvedlist, setApprovedList] = useState([])
+
+
+    const data = [
+        { name: "Total Task", task: list.length},
+        { name: "Approved", task: Approvedlist.length},
+        { name: "Cancel", task: Cancellist.length},
+        { name: "Recycle", task: Cancellist.length},
+
+      ];
 
     useEffect(()=>{
         async function TodoList (){
@@ -64,6 +83,51 @@ const Dashboard = () => {
 
                         </div>
                     </div>
+                </Col>
+
+                <Col className='mt-2' md="4">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                        top: 5,
+                        right: 100,
+                        left: 10,
+                        bottom: 5,
+                        }}
+                        barSize={35}
+                    >
+                    <XAxis
+                        dataKey="name"
+                        scale="point"
+                        padding={{ left: 20, right: 15 }}
+                    />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Bar dataKey="task" fill="#016B64" background={{ fill: "#eee" }} />
+                    </BarChart>
+      
+                </Col>
+
+
+                <Col className='' md="4">
+                    <PieChart width={400} height={400}>
+                        <Pie
+                            dataKey="task"
+                            isAnimationActive={false}
+                            data={data}
+                            cx={200}
+                            cy={150}
+                            outerRadius={80}
+                            fill="#016B64"
+                            label
+                        />
+          <Tooltip />
+                    </PieChart>
+      
                 </Col>
             </Row>
         </Container>
